e92c54364ba349b6dddacb27993c0a15efb75886
hashmi846003 <109304853+hashmi846003@users.noreply.github.com>
2024-12-18 15:38:38 +0530

    First commit

--- channel1.go
+++ channel1.go
@@ -1,11 +1,19 @@
 package main
-import(
+
+import (
 	"fmt"
 )
-func main(){
-	c := make(chan int)
-	go func(){
-		c <- 42
-	}()
-	fmt.Println(<-c)
+
+func main() {
+	ch := make(chan int)
+
+	go func(a, b int) {
+		c := a + b
+		fmt.Println("c value in go routine $", c)
+		ch <- c
+	}(1, 4)
+
+	result, ok := <-ch
+
+	fmt.Printf(" On Main GR got result %d, and status %t \n", result, ok)
 }
